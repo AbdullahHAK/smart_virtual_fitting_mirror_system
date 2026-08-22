cd90eb0b10e35d4d71df882a05ebe6a78d5774c5
Abdullah <116077423+AbdullahHAK@users.noreply.github.com>
2026-08-23 01:34:02 +0500

    box-app: fix missing padding import, was breaking the build

--- box-app/app/src/main/java/com/smartmirror/box/MainActivity.kt
+++ box-app/app/src/main/java/com/smartmirror/box/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.camera.view.PreviewView
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
